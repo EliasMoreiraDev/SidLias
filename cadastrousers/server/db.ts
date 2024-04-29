37aedf15d358bb03fdb1645ab265891ee83a1965
Elias Moreira <121322652+EliasMoreiraDev@users.noreply.github.com>
2024-04-29 10:21:13 -0300

    Delete cadastrousers directory

--- cadastrousers/server/db.ts
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql"
-
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Elias030207",
-    database: "cadastro"
-    
-})
--- cadastrousers/server/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addUser, getUsers } from "./controls/user.js";
-
-const router = express.Router()
-
-router.post("/", addUser)
-router.get("/", getUsers)
-
-export default router
